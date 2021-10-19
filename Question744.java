661f83211767fd78f966744014e2624b5c760c33
vineela2014 <vineela.455@gmail.com>
2021-10-19 15:32:54 +0200

    question744 NextGreatestLetter

---
+++ Question744.java
@@ -0,0 +1,28 @@
+import java.lang.System;
+import java.util.Arrays;
+
+class Question744 {
+    public static void main(String[] args) {
+        char[] input = { 'a', 'c', 'd' };
+        System.out.print(nextGreatestLetter(input, 'b'));
+    }
+
+    public static char nextGreatestLetter(char[] letters, char target) {
+
+        int index = Arrays.binarySearch(letters, target);
+        if (index >= 0) {
+            if (letters.length > index) {
+                return letters[index + 1];
+            } else
+                return letters[0];
+        } else {
+            for (char c : letters) {
+                if (c > target) {
+                    return target;
+                }
+            }
+            return letters[0];
+        }
+
+    }
+}
